17cadb11ef7188e9fe4a61513a774f4c130b6375
Andrew Malov <Andrew.Malov@doubleverify.com>
2020-10-19 13:14:07 +0300

    K8S dashboard is a complete helm chart now

--- helm-tests/k8s-dashboard/install-dashboard-with-rbac.sh
+++ helm-tests/k8s-dashboard/install-dashboard-with-rbac.sh
@@ -1,5 +1,6 @@
 #Get the latest STD resources deployed
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.0.4/aio/deploy/recommended.yaml
-#Enforce RBAC
-kubectl apply -f dashboard-adminuser.yaml
-kubectl apply -f dashboard-cluster-role-binding.yaml
+#wget https://raw.githubusercontent.com/kubernetes/dashboard/v2.0.4/aio/deploy/recommended.yaml
+#reapply the templating over the bundle
+helm install k8s-dashboard .
+
+#find out the NodePort of the kubernetes-dashboard service - and use the bearer token script to obtain the token
