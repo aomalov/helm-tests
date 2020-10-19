eb19c6131e670eab11a01d7d92dbf5ca37bc3895
Andrew Malov <Andrew.Malov@doubleverify.com>
2020-10-20 00:15:22 +0300

    Prometheus operator installed as a chart from the repo   TODO: customize the chart (disable alert manager, limit service monitors, set prometheus and grafana to NodePorts)

---
+++ helm-tests/prometheus-operator/helm-setup.sh
@@ -0,0 +1,10 @@
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm repo add stable https://kubernetes-charts.storage.googleapis.com/
+helm repo update
+#install with helm3 - ignore CRD warnings - looks like everything is OK
+helm install k8s-prom  prometheus-community/kube-prometheus-stack
+# later try to upgrade with values.yaml - to disable some stuff like alert manager
+
+# change grafana service to NodePort - to work via browser  admin:prom-operator
+# navigate to Prometheus using port-forward on the prometheus operated service pod for the port 9090
+
